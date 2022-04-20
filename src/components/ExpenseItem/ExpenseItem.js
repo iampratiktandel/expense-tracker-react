2e0c7485b2e304ded25878748cc8a691c894fdf9
iampratiktandel <iampratiktandel@gmail.com>
2022-04-21 00:29:58 +0530

    feat: add ExpenseList, ExpenseItem & ExpenseDate components

---
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,16 @@
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} month={props.month} year={props.year}/>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>$ {props.amount}</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem;
